051c64dd15c11d03cb3c4b03af9a780448bb41b1
Spixt586 <141528122+Spixt586@users.noreply.github.com>
2026-07-22 19:21:34 -0500

    refactor: correción en formula para el area de un cuadrado

--- Clases/src/com/krakedev/Cuadrado.java
+++ Clases/src/com/krakedev/Cuadrado.java
@@ -4,8 +4,12 @@ public class Cuadrado {
 	public int lado;
 	
 	public double calcularArea() {
-		double areaCuadrado = 4 * lado;
-		
+		double areaCuadrado = lado * lado;
 		return areaCuadrado;
+	}
+	
+	public double calcularPerimetro() {
+		double perimetroCuadrado = (4 * lado);
+		return perimetroCuadrado;
 	}
 }
